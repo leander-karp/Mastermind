1fcf7e15ca14e618a3391c91e78ffcd39168b590
Leander Karp <leander.karp@invision.de>
2020-09-17 10:12:25 +0200

    Add #start method

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
+# The Mastermind class provides the interface to play a game of Master Mind
 class Mastermind
+  def start; end
 end
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -3,4 +3,13 @@
 require 'mastermind'
 
 RSpec.describe Mastermind do
+  context 'given an new game instance' do
+    subject(:game) { described_class.new }
+
+    describe '#start' do
+      it 'asks the player whether he wants to be the codebreaker' do
+        game.start
+      end
+    end
+  end
 end
