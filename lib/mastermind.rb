2166ed2a0b68d90e1a94bc07d57f9dd1879c921c
Leander Karp <leander.karp@invision.de>
2020-09-17 12:38:47 +0200

    Refactor #start to become modular

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -2,13 +2,18 @@
 
 # The Mastermind class provides the interface to play a game of Master Mind
 class Mastermind
+  class Renderer
+    def self.print(msg)
+      puts msg 
+    end 
+  end
   def start
-    puts 'Do you want to be the codebreaker?'
+    Renderer.print('Do you want to be the codebreaker?')
     player_decision = gets.chomp
     if player_decision == 'yes'
-      puts 'Please enter your first guess:'
+      Renderer.print('Please enter your first guess:')
     elsif player_decision == 'no'
-      puts 'Please set your secret code:'
+      Renderer.print('Please set your secret code:')
     end
   end
 end
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -3,38 +3,40 @@
 require 'mastermind'
 
 RSpec.describe Mastermind do
-  context 'given an new game instance' do
-    subject(:game) { described_class.new }
+  subject(:game) { described_class.new }
 
+  context 'player can decide his role' do
     describe '#start' do
       let(:player_role_question) { /Do you want to be the codebreaker?/ }
-      let(:first_guess) { /Please enter your first guess:/}
-      let(:set_secret_code) { /Please set your secret code:/}
+      let(:first_guess) { /Please enter your first guess:/ }
+      let(:set_secret_code) { /Please set your secret code:/ }
 
       context 'given the player does provide invalid input' do
         it 'asks the player whether he wants to be the codebreaker' do
           allow(game).to receive(:gets).and_return('invalid')
-          expect { game.start}.to output(player_role_question).to_stdout
-          expect { game.start}.not_to output(first_guess).to_stdout
-          expect { game.start}.not_to output(set_secret_code).to_stdout 
-        end
-      end 
-      
-      context 'given that the player decides to play as codebreaker' do
-        it 'asks the player to make the first guess' do
-          allow(game).to receive(:gets).and_return('yes')
-          expect { game.start}.not_to output(set_secret_code).to_stdout 
-          expect { game.start}.to output(first_guess).to_stdout
+          expect { game.start }.to output(player_role_question).to_stdout
+          expect { game.start }.not_to output(first_guess).to_stdout
+          expect { game.start }.not_to output(set_secret_code).to_stdout
         end
       end
 
-      context 'given that the player decides to play as codemaker' do
+      context 'if the player decides to play as codebreaker' do
+        it 'asks the player to make the first guess' do
+          allow(game).to receive(:gets).and_return('yes')
+          expect { game.start }.not_to output(set_secret_code).to_stdout
+          expect { game.start }.to output(first_guess).to_stdout
+        end
+      end
+
+      context 'if the player decides to play as codemaker' do
         it 'asks the player to set the secret code' do
           allow(game).to receive(:gets).and_return('no')
-          expect { game.start}.not_to output(first_guess).to_stdout
-          expect { game.start}.to output(set_secret_code).to_stdout
+          expect { game.start }.not_to output(first_guess).to_stdout
+          expect { game.start }.to output(set_secret_code).to_stdout
         end
       end
     end
   end
+
+  
 end
