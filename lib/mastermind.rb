d8395af3a9ed449b69d3bd886c0c31edeecce5e3
Leander Karp <leander.karp@invision.de>
2020-09-18 11:13:57 +0200

    Add guess loop to #start

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -3,12 +3,20 @@
 # The Mastermind class provides the interface to play a game of Master Mind
 class Mastermind
   def start
+    player_guesses
+  end
+
+  private 
+
+  def player_guesses
     puts 'Please enter your next guess:'
     guess = gets.chomp
-    if (1111..6666).include?(guess.to_i)
-      puts 'Correct guess' 
-    else 
-      puts 'Incorrect guess' 
+
+    while !((1111..6666).include?(guess.to_i)) || guess.include?('0')
+      puts 'Invalid guess' 
+      puts 'Please enter your next guess:'
+      guess = gets.chomp
     end
+    puts 'Correct guess' 
   end
 end
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -2,34 +2,45 @@
 
 require 'mastermind'
 
+def generate_guesses(range)
+  colors = range.to_a * 4
+  colors.combination(4).to_a
+end
+
+def generate_invalid_guesses(range)
+  generate_guesses(range).select { |guess| guess.include?(0) }
+end
+
 RSpec.describe Mastermind do
   subject(:game) { described_class.new }
 
   context 'player is codebreaker' do
     describe '#start' do
-      let(:next_guess) { /Please enter your next guess:/}
-      let(:correct_guess) {"\nCorrect guess\n"}
-      let(:incorrect_guess) {"\nIncorrect guess\n"}
-      let(:correct_guesses) {(1111..6666).to_a}
+      let(:next_guess_msg) { "Please enter your next guess:\n"}
+      let(:correct_guess_msg) {"Correct guess\n"}
+      let(:invalid_guess_msg) {"Invalid guess\n"}
 
-      it 'promts the player to guess' do
-        allow(game).to receive(:gets).and_return('1111')
-        expect {game.start}.to output(next_guess).to_stdout
+      it 'asks for guesses until a correct guess is provided' do
+        expected_output = next_guess_msg + invalid_guess_msg + \
+          next_guess_msg + correct_guess_msg
+                  
+        correct_guesses = generate_guesses(1..6)
+        invalid_guesses = generate_invalid_guesses(0..6)
+
+        invalid_guesses.each do |invalid_guess|
+          correct_guess = correct_guesses.sample.join('')
+          allow(game).to receive(:gets).and_return( invalid_guess.join(''), 
+            correct_guess)
+          
+          expect {game.start}.to output(expected_output).to_stdout
+        end
       end
 
-      it 'gets an guess from the player' do 
-        random_guess = correct_guesses.sample.to_s
-        next_guess_msg = next_guess.source
+      it 'displays all guesses made' do 
 
-        allow(game).to receive(:gets).and_return(random_guess, 'abcd')
-
-        expect {game.start}.to output(next_guess_msg + correct_guess).to_stdout
-        expect {game.start}.to output(next_guess_msg + incorrect_guess).to_stdout
-      end 
-
-      it 'asks for guesses until an valid guess is provided' do 
-        
       end 
     end
+
+
   end
 end
