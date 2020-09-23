4c2b7d6459f3d52e1ceba628133419ba5c3c021b
Leander Karp <leander.karp@invision.de>
2020-09-23 14:12:54 +0200

    refactor main.rb to be consistent with new state

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'lib/mastermind'
+require_relative 'lib/renderer'
 
-mastermind = Mastermind.new
+mastermind = Mastermind.new(Renderer.new)
 mastermind.make_player_guess
